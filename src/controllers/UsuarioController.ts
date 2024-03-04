0581049b0feb21f1ca97c7fb18971ed54b8d9243
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-04 19:28:55 -0300

    fix creacion de usuario en la DB

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import { Usermodel } from "../models/UserModel";
 
 
-export const registrar = (req: Request, res: Response) => {
-    res.json({ message: "CREAR usuario" });
+export const registrar = async (req: Request, res: Response) => {
+
+
+    try {
+        const { nombre, email, password } = req.body
+
+        const usuario = new Usermodel(req.body)
+
+        if (nombre === "" || email === "" || password === "") {
+            res.status(400).json({ message: "Todos los campos son rqueridos", usuario })
+            return
+        } else {
+
+            const usuarioAlmacenado = await usuario.save()
+            return usuarioAlmacenado
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
 };
 
